aad5b5a5a5f490b7d5c9fb89421f936d366cd3b0
Lina <117147821+LinA636@users.noreply.github.com>
2024-02-14 14:36:48 +1300

    allow logged in user to create a new event and show it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :created_events, class_name: 'Event', foreign_key: 'user_id'
+  has_many :created_events, class_name: 'Event', foreign_key: 'user_id', dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
