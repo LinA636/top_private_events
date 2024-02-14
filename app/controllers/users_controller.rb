844b8baeddbbc3ac763c3436230f0d611f08340a
Lina <117147821+LinA636@users.noreply.github.com>
2024-02-15 10:23:23 +1300

    have the users show page list all her created events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+    def show
+        @user = current_user
+        puts "Current User ID: #{@user.id}" if @user
+        @events = @user.created_events
+    end
+
 end
