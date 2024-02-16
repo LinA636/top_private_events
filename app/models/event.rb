dce2030dd6ce5ad7e30e64c80a998816df0f96a7
Lina <117147821+LinA636@users.noreply.github.com>
2024-02-16 13:53:36 +1300

    add destroy dependencies to Attendance join table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-    has_many :attendances, foreign_key: 'attended_event_id'
+    has_many :attendances, foreign_key: 'attended_event_id', dependent: :destroy
     has_many :attendees, through: :attendances, source: :attendee
 
     validates :title, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :created_events, class_name: 'Event', foreign_key: 'user_id', dependent: :destroy
-  has_many :attendances, foreign_key: 'attendee_id'
+  has_many :attendances, foreign_key: 'attendee_id', dependent: :destroy
   has_many :attended_events, through: :attendances, source: :attended_event
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
