07455761b956048931e49fd54a22688cc4c4dcd3
Lina <117147821+LinA636@users.noreply.github.com>
2024-02-16 16:59:45 +1300

    allow user to edit their events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,6 +24,27 @@ class EventsController < ApplicationController
         @event = Event.find(params[:id])      
     end
 
+    def edit
+        @event = Event.find(params[:id])
+    end
+
+    def update
+        @event = Event.find(params[:id])
+
+        if @event.update(event_params)
+            redirect_to event_path(@event),notice: 'Event was successfully updated.'
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @event = Event.find(params[:id])
+        @event.destroy
+
+        redirect_to events_path, notice: 'Event was successfully destroyed.'      
+    end
+
     def toggle_attendance
         @event = Event.find(params[:id])
         @user = current_user
